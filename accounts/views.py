845b51d501f1b50902b589ed30b1bd614795db5d
PatrickL32 <Jedi.92.lewis@gmail.com>
2025-07-16 20:44:47 -0700

    7thcommit

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,2 @@
-from django.shortcuts import render
 from django.views.generic import CreateView
-from django.urls import reverse_lazy
-from .forms import CustomUserCreationForm
-
-class SignUpView(CreateView):
-    template_name = "registration/signup.html"
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy("login")
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm
--- posts/views.py
+++ posts/views.py
@@ -7,20 +7,29 @@ from django.views.generic import (
 )
 from posts.models import Status,Post
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import(
+    LoginRequiredMixin,
+    UserPassesTestMixin
+)
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin,CreateView):
     template_name= "posts/new.html"
     model= Post
     fields =[
-        "title","subtitle","author",
+        "title","subtitle",
         "body", "status"
     ]
+
+def form_valid(self,form):
+    form.instance.author = self.request.user
+    return super().form_valid(form)
 
 class PostDetailView(DetailView):
     templete_name = "posts/detail.html"
     model = Post
 
-class PostUpdateView(UpdateView):
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
     template_name = "posts/edit.html"
     model= Post
     fields =[
@@ -28,15 +37,46 @@ class PostUpdateView(UpdateView):
         "status"
     ]
 
+def test_func(self):
+    post = self.get_object()
+    return post.author==self.request.user
+
 class PostDeleteView(DeleteView):
-    template_name ="posts/delete.html"
+    template_name ="posts/list.html"
     model=Post
     success_url = reverse_lazy("list")
 
 class PostListView(ListView):
     template_name = "posts/list.html"
+    model=Post
+
+class DraftPostListView(LoginRequiredMixin, ListView):
+    template_name = "posts/list.html"
+    model=Post 
+    
+def get_context_data(self,**kwargs):
+    context = super().get_context_data(**kwargs)
+    draft = Status.objects.get(name="draft")
+    context["title"]="Draft"
+    context["post_list"] =(
+        Post.objects
+        .filter(status= draft)
+        .filter(author=self.request.user)
+        .order_by("created_on").reverse()
+    )
+    return context
+
+class ArchievedPostListView( LoginRequiredMixin,ListView):
+    template_name = "posts/list.html"
     model = Post
 
-
-
-# Create your views here.
+def get_caught_data(self,**kwargs):
+    context= super().get_caught_data(**kwargs)
+    archived = Status.objects.get(name="archived")
+    context["title"] = "Archived"
+    context=["post_list"] =(
+        Post.objects
+        .filter(status=archived)
+        .order_by("created_on").reverse()
+    )
+    return context
